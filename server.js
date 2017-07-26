2483bcd385524b0e196091c6216dd833696061a0
Prashant Gupta <antshpra@gmail.com>
2017-07-26 15:17:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,24 +36,14 @@ const archiveConfig = {
       sortBy:'FOLLOW_DATE', minValue:new Date(0),
       batchSize:1000, minUpdate:1000,
       timeInt:900, minTimeInt:300, maxTimeInt:3600, nextRun:0, boost:100 },
-  USER_PRATILIPI_201704:{
-      kind:'USER_PRATILIPI', schema:require( `./schema/USER_PRATILIPI.js` ), fileName:'USER_PRATILIPI-2017.04',
-      sortBy:'_TIMESTAMP_', minValue:new Date(1490985000000), maxValue:new Date(1493577000000),
-      batchSize:1000, minUpdate:1000,
-      timeInt:900, minTimeInt:300, maxTimeInt:3600, nextRun:0, boost:100 },
-  USER_PRATILIPI_201705:{
-      kind:'USER_PRATILIPI', schema:require( `./schema/USER_PRATILIPI.js` ), fileName:'USER_PRATILIPI-2017.05',
-      sortBy:'_TIMESTAMP_', minValue:new Date(1493577000000), maxValue:new Date(1496255400000),
-      batchSize:1000, minUpdate:1000,
-      timeInt:900, minTimeInt:300, maxTimeInt:3600, nextRun:0, boost:100 },
-  USER_PRATILIPI_201706:{
-      kind:'USER_PRATILIPI', schema:require( `./schema/USER_PRATILIPI.js` ), fileName:'USER_PRATILIPI-2017.06',
-      sortBy:'_TIMESTAMP_', minValue:new Date(1496255400000), maxValue:new Date(1498847400000),
-      batchSize:1000, minUpdate:1000,
-      timeInt:900, minTimeInt:300, maxTimeInt:3600, nextRun:0, boost:100 },
   USER_PRATILIPI_201707:{
       kind:'USER_PRATILIPI', schema:require( `./schema/USER_PRATILIPI.js` ), fileName:'USER_PRATILIPI-2017.07',
       sortBy:'_TIMESTAMP_', minValue:new Date(1498847400000), maxValue:new Date(1501525800000),
+      batchSize:1000, minUpdate:1000,
+      timeInt:900, minTimeInt:300, maxTimeInt:3600, nextRun:0, boost:100 },
+  USER_PRATILIPI_201708:{
+      kind:'USER_PRATILIPI', schema:require( `./schema/USER_PRATILIPI.js` ), fileName:'USER_PRATILIPI-2017.08',
+      sortBy:'_TIMESTAMP_', minValue:new Date(1501525800000), maxValue:new Date(1504204200000),
       batchSize:1000, minUpdate:1000,
       timeInt:900, minTimeInt:300, maxTimeInt:3600, nextRun:0, boost:100 }
 };
