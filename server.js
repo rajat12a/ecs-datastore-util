4821773e46bcd358edcd903288bf279812380048
Rajat Agarwal <rajat12a@gmail.com>
2017-09-02 18:32:18 +0530

    Changed node version and changed timeLimit and also removed backup for two files

--- server.js
+++ server.js
@@ -31,29 +31,9 @@ const archiveConfig = {
       sortBy:'_TIMESTAMP_', minValue:new Date(0),
       batchSize:1000, minUpdate: 100,
       timeInt:900, minTimeInt:300, maxTimeInt: 900, nextRun:0, boost: 10 },
-  USER_AUTHOR:{
-      kind:'USER_AUTHOR', schema:require( `./schema/USER_AUTHOR.js` ), fileName:'USER_AUTHOR',
-      sortBy:'FOLLOW_DATE', minValue:new Date(0),
-      batchSize:1000, minUpdate:1000,
-      timeInt:900, minTimeInt:300, maxTimeInt:3600, nextRun:0, boost:100 },
-  USER_PRATILIPI_201708:{
-      kind:'USER_PRATILIPI', schema:require( `./schema/USER_PRATILIPI.js` ), fileName:'USER_PRATILIPI-2017.08',
-      sortBy:'_TIMESTAMP_', minValue:new Date(1501525800000), maxValue:new Date(1504204200000),
-      batchSize:1000, minUpdate:1000,
-      timeInt:900, minTimeInt:300, maxTimeInt:3600, nextRun:0, boost:100 },
   USER_PRATILIPI_201709:{
       kind:'USER_PRATILIPI', schema:require( `./schema/USER_PRATILIPI.js` ), fileName:'USER_PRATILIPI-2017.09',
       sortBy:'_TIMESTAMP_', minValue:new Date(1504204200000), maxValue:new Date(1506796200000),
-      batchSize:1000, minUpdate:1000,
-      timeInt:900, minTimeInt:300, maxTimeInt:3600, nextRun:0, boost:100 },
-  VOTE:{
-      kind:'VOTE', schema:require( `./schema/VOTE.js` ), fileName:'VOTE',
-      sortBy:'CREATION_DATE', minValue:new Date(0),
-      batchSize:1000, minUpdate:1000,
-      timeInt:900, minTimeInt:300, maxTimeInt:3600, nextRun:0, boost:100 },
-  COMMENT:{
-      kind:'COMMENT', schema:require( `./schema/COMMENT.js` ), fileName:'COMMENT',
-      sortBy:'CREATION_DATE', minValue:new Date(0),
       batchSize:1000, minUpdate:1000,
       timeInt:900, minTimeInt:300, maxTimeInt:3600, nextRun:0, boost:100 }
 };
@@ -93,7 +73,7 @@ const bigqueryConfig = {
       if( err ) {
         console.error( archive + ": " + String( err ) );
       } else {
-        if( updateCount === config.batchSize ) {
+        if( updateCount >= config.batchSize ) {
           config.timeInt = Math.max( config.minTimeInt, Math.ceil( config.timeInt / 2 ) );
         } else if( updateCount < config.minUpdate ) {
           config.timeInt = Math.min( config.maxTimeInt, config.timeInt * 2 );
