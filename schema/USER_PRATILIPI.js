61a3536a8192c9daa9bc20a64226974e75aa20f7
Rajat Agarwal <rajat12a@gmail.com>
2017-10-10 19:38:59 +0530

    [CHANGE] library backup

--- schema/USER_PRATILIPI.js
+++ schema/USER_PRATILIPI.js
@@ -2,18 +2,13 @@ module.exports = {
 
 	  structure: {
 		      USER_PRATILIPI_ID:	{ type: 'STRING',    default: null },
+    		  ADDED_TO_LIB:	{ type: 'BOOLEAN',   default: null },
+    		  ADDED_TO_LIB_DATE:	{ type: 'TIMESTAMP', default: null },
 		      PRATILIPI_ID:	{ type: 'INTEGER',   default: null },
-		      RATING:		{ type: 'INTEGER',   default: null },
-		      RATING_DATE:	{ type: 'TIMESTAMP', default: null },
-		      REVIEW_DATE:	{ type: 'TIMESTAMP', default: null },
-		      REVIEW_STATE:	{ type: 'STRING',    default: null },
 		      USER_ID:		{ type: 'INTEGER',   default: null },
-		      _TIMESTAMP_:	{ type: 'TIMESTAMP', default: null },
-		      REVIEW_TITLE:	{ type: 'STRING',    default: null },
-		      REVIEW:		{ type: 'STRING',    default: null }
+		      _TIMESTAMP_:	{ type: 'TIMESTAMP', default: null }
 		    },
 
 	  primaryKey: 'USER_PRATILIPI_ID'
 
 };
-
