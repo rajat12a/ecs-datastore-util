4c391df43c799248c7777e2715810ca0817fd90d
Rajat Agarwal <rajat12a@gmail.com>
2017-10-27 16:05:35 +0530

    [CHANGE] migrating accessToken

---
+++ schema/ACCESS_TOKEN.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+	  structure: {
+		      ACCESS_TOKEN_ID:	{ type: 'STRING',    default: null },
+    		  FCM_TOKEN:	{ type: 'STRING',   default: null },
+		      USER_ID:		{ type: 'INTEGER',   default: null }
+		    },
+
+	  primaryKey: 'ACCESS_TOKEN_ID'
+
+};
