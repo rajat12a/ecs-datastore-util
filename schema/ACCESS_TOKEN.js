a3d2f6b3675708dc36c31c3294a160a204be4b3a
Rajat Agarwal <rajat12a@gmail.com>
2017-10-31 12:22:59 +0530

    [CHANGE] Schema

--- schema/ACCESS_TOKEN.js
+++ schema/ACCESS_TOKEN.js
@@ -2,6 +2,7 @@ module.exports = {
 
 	  structure: {
 		      ACCESS_TOKEN_ID:	{ type: 'STRING',    default: null },
+		      CREATION_DATE:	{ type: 'TIMESTAMP',    default: null },
     		  FCM_TOKEN:	{ type: 'STRING',   default: null },
 		      USER_ID:		{ type: 'INTEGER',   default: null }
 		    },
