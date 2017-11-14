48f5ec6e06d5e270e55a527cd862c5aafd7d45c8
Rajat Agarwal <rajat12a@gmail.com>
2017-11-14 19:44:49 +0530

    [CHANGE] Add Author backup in multiple file

---
+++ schema/AUTHOR.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+  structure: {
+    AUTHOR_ID:                  { type: 'INTEGER',   default: null },
+    USER_ID:                    { type: 'INTEGER',   default: null },
+    LANGUAGE:                   { type: 'STRING',    default: null },
+  },
+
+  primaryKey: 'AUTHOR_ID'
+
+};
