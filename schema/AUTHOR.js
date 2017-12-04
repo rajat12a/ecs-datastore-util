bc10621000a8534e12a681bd064f282cb925c2d5
Rajat Agarwal <rajat12a@gmail.com>
2017-12-04 21:35:58 +0530

    [CHANGE] Author backup with time or id

--- schema/AUTHOR.js
+++ schema/AUTHOR.js
@@ -2,8 +2,32 @@ module.exports = {
 
   structure: {
     AUTHOR_ID:                  { type: 'INTEGER',   default: null },
-    USER_ID:                    { type: 'INTEGER',   default: null },
+    CONTENT_DRAFTED:            { type: 'INTEGER',   default: 0    },
+    CONTENT_PUBLISHED:          { type: 'INTEGER',   default: 0    },
+    COVER_IMAGE:                { type: 'STRING',    default: null },
+    DATE_OF_BIRTH:              { type: 'STRING',    default: null },
+    FIRST_NAME:                 { type: 'STRING',    default: null },
+    FIRST_NAME_EN:              { type: 'STRING',    default: null },
+    FOLLOW_COUNT:               { type: 'INTEGER',   default: 0    },
+    GENDER:                     { type: 'STRING',    default: null },
     LANGUAGE:                   { type: 'STRING',    default: null },
+    LAST_NAME:                  { type: 'STRING',    default: null },
+    LAST_NAME_EN:               { type: 'STRING',    default: null },
+    LAST_UPDATED:               { type: 'TIMESTAMP', default: null },
+    LOCATION:                   { type: 'STRING',    default: null },
+    PEN_NAME:                   { type: 'STRING',    default: null },
+    PEN_NAME_EN:                { type: 'STRING',    default: null },
+    PROFILE_FACEBOOK:           { type: 'STRING',    default: null },
+    PROFILE_GOOGLE_PLUS:        { type: 'STRING',    default: null },
+    PROFILE_IMAGE:              { type: 'STRING',    default: null },
+    PROFILE_TWITTER:            { type: 'STRING',    default: null },
+    REGISTRATION_DATE:          { type: 'TIMESTAMP', default: null },
+    STATE:                      { type: 'STRING',    default: null },
+    SUMMARY:                    { type: 'STRING',    default: null },
+    TOTAL_FB_LIKE_SHARE_COUNT:  { type: 'INTEGER',   default: 0    },
+    TOTAL_READ_COUNT:           { type: 'INTEGER',   default: 0    },
+    USER_ID:                    { type: 'INTEGER',   default: null },
+    _TIMESTAMP_:                { type: 'TIMESTAMP', default: null } 
   },
 
   primaryKey: 'AUTHOR_ID'
---
+++ server-author-time.js
@@ -0,0 +1,39 @@
+const JsonArchive = require( './JsonArchive-author-time.js' );
+
+const archiveConfig = {
+   AUTHOR:{
+      kind:'AUTHOR', schema:require( `./schema/AUTHOR.js` ), fileName:'AUTHOR/AUTHOR_TIME',
+      sortBy:'_TIMESTAMP_', minValue:new Date(0),
+      batchSize:1000, minUpdate:1000,
+      timeInt:900, minTimeInt:300, maxTimeInt:3600, nextRun:0, boost:1000000 }
+};
+
+var archives = Object.keys( archiveConfig );
+for( var i = 0; i < archives.length; i++ ) {
+  (function run() {
+    var archive = archives[ i ];
+    var config = JSON.parse(JSON.stringify(archiveConfig[ archive ]));
+    var callback = ( err, updateCount ) => {
+      if( err ) {
+        console.error( "RUN: " + String( err ) );
+      } else {
+        console.log(`RUN: complete`);
+      }
+    };
+    if( archive === 'USER_AUTHOR' || archive === 'USER_PRATILIPI' ) {
+      for( var j = 4500000000000000; j < 9000000000000000; j+=500000000000000 ) {
+        var configSplit = JSON.parse(JSON.stringify(config));
+        configSplit.minValue = j;
+        configSplit.maxValue = ( j + 500000000000000 );
+        configSplit.fileName = configSplit.fileName + `_${configSplit.minValue}_${configSplit.maxValue}`;
+        var jsonArchive = new JsonArchive();
+        console.log(`${archive + '_' + configSplit.minValue + '_' + configSplit.maxValue}: Taking Backup`);
+        jsonArchive.run( (archive + '_' + configSplit.minValue + '_' + configSplit.maxValue), configSplit, callback );
+      }
+    } else {
+      var jsonArchive = new JsonArchive();
+      console.log(`${archive}: Taking Backup`);
+      jsonArchive.run( archive, config, callback );
+    }
+  })();
+}
